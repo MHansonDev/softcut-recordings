909ad1737f8abe3177013da9638a555b9319edb1
MHansonDev <mhanson048@gmail.com>
2022-08-29 19:58:05 -0400

    Added Audio Tooltip
    Introduction of an Audio File tooltip on hover.

--- softcut-ui/src/app/controls/media-player/media-player.component.ts
+++ softcut-ui/src/app/controls/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { AudioService } from 'src/app/archive/audio/audio.service';
 import { AudioFile } from './audio-file.model';
 
 @Component({
@@ -10,18 +11,30 @@ import { AudioFile } from './audio-file.model';
 export class MediaPlayerComponent {
 
     @ViewChild('audioRef', { static: true }) audioRef: ElementRef<HTMLAudioElement>;
+    @ViewChild('audioInfo', {static: true }) audioInfoRef: ElementRef<HTMLDivElement>;
     @Input() files: Array<any> = [];
     currentFile: any = {};
 
     constructor(
+        private audioService: AudioService
     ) { }
 
     ngAfterViewInit(): void {
     }
 
-    openFile(file: AudioFile, i: number) {
+    openFile(file: AudioFile) {
         this.audioRef.nativeElement.src = file.path;
         this.audioRef.nativeElement.play();
+    }
+
+    fileMouseEnter(file: AudioFile) {
+        let audioInfo = this.audioService.audioInfo.filter(ai => ai.id == file.id)[0];
+        this.audioInfoRef.nativeElement.style.display = 'block';
+        this.audioInfoRef.nativeElement.innerHTML = '<span>Description: ' + audioInfo.description + '</span>'
+    }
+
+    fileMouseLeave() {
+        this.audioInfoRef.nativeElement.style.display = 'none';
     }
 
 }
